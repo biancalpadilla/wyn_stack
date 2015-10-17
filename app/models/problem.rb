d41ebca8b79c6c7f205700be79b74b86a56efccc
Bianca Padilla <biancalpadilla@gmail.com>
2015-10-17 18:16:30 -0400

    Problem model, add css, add root route, add navbar

--- app/models/problem.rb
+++ app/models/problem.rb
@@ -1,2 +1,6 @@
 class Problem < ActiveRecord::Base
+  validates :title, :description, :name, presence: true
+  validates :title, length: { in: 10..55 }
+  validates :description, length: { in: 10..600 }
+  validates :name, length: { in: 3..17 }
 end
