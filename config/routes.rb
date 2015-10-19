3735df11b67249e02e801c7be5787c909f661434
Bianca Padilla <biancalpadilla@gmail.com>
2015-10-19 18:53:35 -0400

    Fixed indentation

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations' }
   resources :comments
   resources :problems
-   root :to => redirect('/problems')
+  root :to => redirect('/problems')
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
